23ae5dcea2bc36e6f30c9d9d8ce6b8bc06a9882f
Monisha Ranjan <ranjan.monisha233@gmail.com>
2025-06-20 08:30:32 -0400

    Revision/Day2

--- Revision/Day2/Majority Element-II.cpp
+++ Revision/Day2/Majority Element-II.cpp
@@ -46,11 +46,70 @@ public:
     }
 };
 
-or this 
+// or this 
+
+class Solution {
+public:
+    vector<int> majorityElementTwo(vector<int>& nums) {
+        map<int,int> mp;
+        vector<int> ans;
+        int maj = nums.size()/3+1;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]]==maj){
+                ans.push_back(nums[i]);
+                mp[nums[i]]=INT_MIN;
+            }
+            if(ans.size()==2) break;
+        }
+        return ans;
+    }
+};
+
+// optimal soln - remove the extra space of map , in worst case it will O(n)
 
 
+class Solution {
+public:
+    vector<int> majorityElementTwo(vector<int>& nums) {
+       int ele1 =0,ele2=0;
+       int count1=0,count2=0;
+       
+       int n = nums.size();
 
-// optimal soln
+       for(int i=0;i<n;i++){
+            if(count1==0 and nums[i]!=ele2){
+                ele1 = nums[i];
+                count1=1;
+            }
+            else if(count2==0 and nums[i]!=ele1){
+                ele2 = nums[i];
+                count2 = 1;
+            }
+            else if(ele1==nums[i]) count1++;
+            else if(ele2==nums[i]) count2++;
+            else{
+                count1--;
+                count2--;
+            }
+       }
+       vector<int> ans;
+
+       //check for answer
+       count1=0;
+       count2=0;
+       for(int i=0;i<n;i++){
+            if(nums[i]==ele1) count1++;
+            else if(nums[i]==ele2) count2++;
+       }
+       if(count1> n/3) ans.push_back(ele1);
+       if(count2> n/3) ans. push_back(ele2);
+       
+       return ans;
+    }
+};
+
 
 
 
